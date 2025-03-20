bca69a72bdfa6ff51efc2f0c1adbf3b45cbc533f
Ujwal Reddy Battu <168129796+ujwalreddybattu04@users.noreply.github.com>
2025-03-20 09:36:18 +0530

    Time: 0 ms (100%), Space: 17.9 MB (38.79%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -2,30 +2,29 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix or not matrix[0]:  # Check for empty matrix
+        if not matrix or not matrix[0]:  
             return []
         
         result = []
         top, bottom, left, right = 0, len(matrix) - 1, 0, len(matrix[0]) - 1
         
         while top <= bottom and left <= right:
-            # Traverse from left to right
+           
             for i in range(left, right + 1):
                 result.append(matrix[top][i])
             top += 1
 
-            # Traverse from top to bottom
             for i in range(top, bottom + 1):
                 result.append(matrix[i][right])
             right -= 1
 
-            # Traverse from right to left (if there is a new row)
+            
             if top <= bottom:
                 for i in range(right, left - 1, -1):
                     result.append(matrix[bottom][i])
                 bottom -= 1
 
-            # Traverse from bottom to top (if there is a new column)
+          
             if left <= right:
                 for i in range(bottom, top - 1, -1):
                     result.append(matrix[i][left])
