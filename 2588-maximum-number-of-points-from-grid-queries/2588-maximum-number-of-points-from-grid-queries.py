843ba31b0e209bc9e1d9dd0e31a00eab22de3889
Ujwal Reddy Battu <168129796+ujwalreddybattu04@users.noreply.github.com>
2025-03-28 22:41:10 +0530

    Time: 632 ms (64.05%), Space: 43.8 MB (11.11%) - LeetHub

---
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class IndexedQuery:
+  queryIndex: int
+  query: int
+
+  def __iter__(self):
+    yield self.queryIndex
+    yield self.query
+
+
+class Solution:
+  def maxPoints(self, grid: list[list[int]], queries: list[int]) -> list[int]:
+    DIRS = ((0, 1), (1, 0), (0, -1), (-1, 0))
+    m = len(grid)
+    n = len(grid[0])
+    ans = [0] * len(queries)
+    minHeap = [(grid[0][0], 0, 0)]
+    seen = {(0, 0)}
+    accumulate = 0
+
+    for queryIndex, query in sorted([IndexedQuery(i, query)
+                                     for i, query in enumerate(queries)],
+                                    key=lambda x: x.query):
+      while minHeap:
+        val, i, j = heapq.heappop(minHeap)
+        if val >= query:
+          heapq.heappush(minHeap, (val, i, j))
+          break
+        accumulate += 1
+        for dx, dy in DIRS:
+          x = i + dx
+          y = j + dy
+          if x < 0 or x == m or y < 0 or y == n:
+            continue
+          if (x, y) in seen:
+            continue
+          heapq.heappush(minHeap, (grid[x][y], x, y))
+          seen.add((x, y))
+      ans[queryIndex] = accumulate
+
+    return ans
